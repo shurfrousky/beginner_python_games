7c04e157206ac7019c638f8c1c473b3cffdfd755
Jeffrey Norris <jeffreydn1161@gmail.com>
2022-10-10 11:38:00 -0400

    Adding the directory for a new password project.

---
+++ Games/Password_Generator/README.txt
@@ -0,0 +1,10 @@
+The Goal: Provide various utilities for generating, modifying, and checking passwords.
+
+This project will have the following options for a user:
+1. Generate a new password
+    a. Select its length
+    b. Select upper/lower/numbers/symbols
+
+2. Mutate an existing password into 1337speak
+
+3. Test the strength of a password.
---
+++ Games/Password_Generator/generator.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+# Take user input for type of password
+""" Must have upper or lowercase selected
+Password Length 
+bool Uppercase string.ascii_upercase
+bool Lowercase string.ascii_lowercase
+bool Numbers [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+bool Symbols ['!', '(', ')', '?', '[', ']', '_', '`', '~', ';', ':', '@', '#', '$', '%', '^', '&', '*', '+', '=']
+"""
+# letter = random.randint(0, 26)
+# print (string.ascii_lowercase[letter])
+
+# Generate password
+"""
+password = ""
+Always start with a letter
+randomly choose upper, lower
+    append to password
+loop length number times -1
+randomly choose upper, lower, number, letter
+    append to password
+"""
+
+""" -------------------------------------------- """
+# Extra
+
+# Output the strength of the password
+# https://www.codespeedy.com/check-the-password-strength-in-python/#:~:text=match('((%5Cd,input%20given%20by%20the%20user).
+
+# Mutate a password into l33tspeak
+
+# check strength of a given password.
+
